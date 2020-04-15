0cba43d8c3604cacfb1ee71afcf938ea2e0fcec8
Matija Nalis <mnalis-git-torres@data.voyager.hr>
2020-04-15 20:38:44 +0200

    parse collections

--- mg_to_static.pl
+++ mg_to_static.pl
@@ -1,28 +1,64 @@
 #!/usr/bin/perl
 # Matija Nalis <mnalis-perl@voyager.hr> GPLv3+ started 2020-04-15
 # converts data from Mediagoblin instance to static html
+#
+# run as: 
+#   sudo -u postgres ./mg_to_static.pl > runme.sh && sh runme.sh
+#
 
 use warnings;
 use strict;
-use DBI;
 use autodie qw/:all/;
-use Data::Dumper;
-
 use feature 'say';
+
+use DBI;
+use Data::Dumper;
+use HTML::Template;
+use FindBin qw( $RealBin );
 
 my $DB_NAME = 'mediagoblin';
 my $MG_ROOT = '/var/lib/mediagoblin/default/media/public/media_entries';
+my $NEW_ROOT = './mg_html';
 
 #
 # no user serviceable parts below
 #
 
+# creates directory if it doesn't exists
+sub do_mkdir ($) {
+    my ($dir) = @_;
+    mkdir $dir unless -d $dir;
+}
+
+# create whole collection
+sub create_collection($) {
+    my ($c) = @_;
+
+    my $collection_template = HTML::Template->new(path => $RealBin, filename => 'collection.tmpl', utf8 => 1);
+
+    $collection_template->param(
+        title => $$c{'title'},
+        description => $$c{'description'},
+    );
+
+    print $collection_template->output;
+    die;
+}
+
+#
+# main
+#
+
 my $dbh = DBI->connect("dbi:Pg:dbname=$DB_NAME", '', '', {AutoCommit => 0, RaiseError => 1});
 
-my $sth = $dbh->prepare('SELECT * from core__users');
-$sth->execute();
+do_mkdir ($NEW_ROOT);
+chdir $NEW_ROOT or die "can't chdir to $NEW_ROOT: $!";
 
-say Dumper($sth->fetchall_hashref('username'));
+my $collection_sth = $dbh->prepare('SELECT core__collections.id, title, slug, core__users.username, description FROM core__collections LEFT JOIN core__users ON core__collections.creator = core__users.id;');
+$collection_sth->execute();
 
+while (my $collection = $collection_sth->fetchrow_hashref) {
+    create_collection ($collection);
+}
 
 $dbh->disconnect;
