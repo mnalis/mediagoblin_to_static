30e7599c0c31ed298dd688d8952a4e8d95a51022
Matija Nalis <mnalis-git-torres@data.voyager.hr>
2020-04-15 19:49:31 +0200

    test DBD::Pg

--- mg_to_static.pl
+++ mg_to_static.pl
@@ -4,4 +4,25 @@
 
 use warnings;
 use strict;
+use DBI;
+use autodie qw/:all/;
+use Data::Dumper;
 
+use feature 'say';
+
+my $DB_NAME = 'mediagoblin';
+my $MG_ROOT = '/var/lib/mediagoblin/default/media/public/media_entries';
+
+#
+# no user serviceable parts below
+#
+
+my $dbh = DBI->connect("dbi:Pg:dbname=$DB_NAME", '', '', {AutoCommit => 0, RaiseError => 1});
+
+my $sth = $dbh->prepare('SELECT * from core__users');
+$sth->execute();
+
+say Dumper($sth->fetchall_hashref('username'));
+
+
+$dbh->disconnect;
