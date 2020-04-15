4c5f9ec8f09af331ef12814a829bc3d71ba66b91
Matija Nalis <mnalis-git-torres@data.voyager.hr>
2020-04-15 21:47:01 +0200

    media parsing

--- mg_to_static.pl
+++ mg_to_static.pl
@@ -7,6 +7,7 @@
 #
 
 # FIXME zali se na UTF8 "Wide character in print" , zasto
+# FIXME check da li ima fileova u $MG_ROOT koje nismo referencirali u $NEW_ROOT
 
 use warnings;
 use strict;
@@ -28,6 +29,8 @@ my $NEW_ROOT = './mg_html';
 # no user serviceable parts below
 #
 
+my $dbh;
+
 # creates directory if it doesn't exists
 sub do_mkdir ($) {
     my ($dir) = @_;
@@ -40,14 +43,26 @@ sub create_collection($) {
 
     my $collection_template = HTML::Template->new(path => $RealBin, filename => 'collection.tmpl', utf8 => 1);
 
-    $$c{'description'} =~ s{\[(.+?)\]\s*\((.+?)\)}{<A HREF="$2">$1</A>}gi;	# replace HTTP linkove da ih pretvori u A HREF
+    $$c{'description'} =~ s{\[(.+?)\]\s*\((.+?)\)}{<A HREF="$2">$1</A>}gi;	# convert HTTP links to <A HREF>
 
+    # template headers
     $collection_template->param(
         title => $$c{'title'},
         description => $$c{'description'},
     );
+    
+    # template loop for each picture
+    my $one_collection_sth = $dbh->prepare ("SELECT core__media_entries.id,  core__media_entries.title, core__media_entries.slug, core__media_entries.description FROM core__collection_items LEFT JOIN core__media_entries ON core__media_entries.id = core__collection_items.media_entry WHERE collection=? ORDER BY position, core__collection_items.id");
+    $one_collection_sth->execute($$c{'id'});
+    
+    while (my $media = $one_collection_sth->fetchrow_hashref) {
+        say "collection=$$c{id} media=$$media{id} title=$$media{title} slug=$$media{slug} desc=$$media{description}";
+    }
 
-    my $c_dir = "./u/$$c{'username'}/collection/$$c{'slug'}";
+
+
+    # create index.html
+    my $c_dir = "./u/$$c{username}/collection/$$c{slug}";
     do_mkdir ($c_dir);
 
     open my $c_index, '>', "$c_dir/index.html";
@@ -59,15 +74,15 @@ sub create_collection($) {
 # main
 #
 
-my $dbh = DBI->connect("dbi:Pg:dbname=$DB_NAME", '', '', {AutoCommit => 0, RaiseError => 1});
+$dbh = DBI->connect("dbi:Pg:dbname=$DB_NAME", '', '', {AutoCommit => 0, RaiseError => 1});
 
 do_mkdir ($NEW_ROOT);
 chdir $NEW_ROOT or die "can't chdir to $NEW_ROOT: $!";
 
-my $collection_sth = $dbh->prepare('SELECT core__collections.id, title, slug, core__users.username, description FROM core__collections LEFT JOIN core__users ON core__collections.creator = core__users.id;');
-$collection_sth->execute();
+my $collections_sth = $dbh->prepare('SELECT core__collections.id, title, slug, core__users.username, description FROM core__collections LEFT JOIN core__users ON core__collections.creator = core__users.id;');
+$collections_sth->execute();
 
-while (my $collection = $collection_sth->fetchrow_hashref) {
+while (my $collection = $collections_sth->fetchrow_hashref) {
     create_collection ($collection);
 }
 
